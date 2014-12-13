6ecfea2d58f953602179a447623ea599a7141315
vivk <vivk@debian>
2014-12-13 20:29:03 +0200

    Вместо print_r подключенна библиотека var-dumper

---
+++ Resources/functions/debug.php
@@ -0,0 +1,32 @@
+<?php
+
+use DebugFunctions\Dumper;
+
+/**
+ * Вывести debug информацию
+ * @param array $array
+ * @return void
+ */
+function pre()
+{
+	if (!headers_sent()) {
+		header('Content-type:text/html; charset=utf-8');
+	}
+
+	(new Dumper)->dump();
+}
+
+/**
+ * Вывести debug информацию и прекратить работу скрипта
+ * @param array $array
+ * @return void
+ */
+function pred()
+{
+	if (!headers_sent()) {
+		header('Content-type:text/html; charset=utf-8');
+	}
+
+	(new Dumper)->dump();
+	die;
+}
---
+++ src/DebugFunctions/Dumper.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace DebugFunctions;
+
+use Symfony\Component\VarDumper\VarDumper;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
+
+/**
+ * Класс реализующий стандартный dump с переопределенным обработчиком.
+ * Переопределенный обработчик снимает ограничение на уровень вложености.
+ */
+class Dumper
+{
+	private $_handler;
+
+	/**
+	 * Вывести debug информацию
+	 * @return \DebugFunctions\Dumper
+	 */
+	public function dump()
+	{
+		return $this->_initHandler()
+			->_dumpByArgList(func_get_args());
+	}
+
+	/**
+	 * Установить обработчик
+	 * @param \callable $callable
+	 * @return \DebugFunctions\Dumper
+	 * @throws \InvalidArgumentException
+	 */
+    public function setHandler(\callable $callable)
+    {
+        if (null !== $callable && !is_callable($callable, true)) {
+            throw new \InvalidArgumentException('Invalid PHP callback.');
+        }
+
+        $this->_handler = $callable;
+
+        return $this;
+    }
+
+	/**
+	 * Инициализировать обработчик
+	 * @return \DebugFunctions\Dumper
+	 */
+	protected function _initHandler()
+	{
+		if (null === $this->_handler) {
+			$this->_handler = function ($var) {
+				$cloner = new VarCloner();
+				$cloner->setMaxItems(-1);
+				$dumper = 'cli' === PHP_SAPI ? new CliDumper() : new HtmlDumper();
+				$dumper->dump($cloner->cloneVar($var));
+			};
+		}
+
+		VarDumper::setHandler($this->_handler);
+
+		return $this;
+	}
+
+	/**
+	 * Вывести debug информацию по списку переменных
+	 * @param array $arg_list
+	 * @return \DebugFunctions\Dumper
+	 */
+	protected function _dumpByArgList(array $arg_list)
+	{
+		foreach ($arg_list as $var) {
+			VarDumper::dump($var);
+		}
+
+		return $this;
+	}
+}
